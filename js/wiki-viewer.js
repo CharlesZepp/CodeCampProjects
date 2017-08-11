5c8ffe522153ac5102291b5d9a459aa3c0518215
CharlesZepp <zepp1@umbc.edu>
2017-08-11 14:07:07 -0400

    commit wiki-search code clean-up

--- js/wiki-viewer.js
+++ js/wiki-viewer.js
@@ -7,32 +7,32 @@ var results;
 
 //AJAX request for Wiki API based on search box value
 function search(){
-	var searchVal = $('#search-box').val();
-	results ="";
+  var searchVal = $('#search-box').val();
+  results ="";
 
-	if (searchVal !== ""){ //search box is not empty
+  if (searchVal !== ""){ //search box is not empty
 
-		$.ajax({
+    $.ajax({
 
-			url: api+searchVal+cb,
-			type: 'GET',
-			dataType: 'json',
-			async: false,
-			contentType: "application/json; charset=utf-8",
+      url: api+searchVal+cb,
+      type: 'GET',
+      dataType: 'json',
+      async: false,
+      contentType: "application/json; charset=utf-8",
 
-			success: function(data){
-				//console.log(data);
-				if(data[1].length == 0){results = "No Results Found";} // no data
-				else
-					for(var i =0; i < data[1].length; i++){
+      success: function(data){
+        //console.log(data);
+        if(data[1].length == 0){results = "No Results Found";} // no data
+        else
+          for(var i =0; i < data[1].length; i++){
 
-						results +='<div class="search-results"><a class="block" href="'+data[3][i]+
-						'"><div class="results-name">'+data[1][i]+
-						'</div><div class="results-desc">'+data[2][i]+'</div></a></div>';
-					}
-					$('#view').html(results);
-						}//success
-				});//ajax
-	}
+            results +='<div class="search-results"><a class="block" href="'+data[3][i]+
+            '"><div class="results-name">'+data[1][i]+
+            '</div><div class="results-desc">'+data[2][i]+'</div></a></div>';
+          }
+          $('#view').html(results);
+            }//success
+        });//ajax
+  }
 }//search
 
